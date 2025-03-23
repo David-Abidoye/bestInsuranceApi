e024f90a924a309b9b6c32139bd0e165128807bd
David-Abidoye <akinloluabidoye.aa@gmail.com>
2025-03-23 18:58:08 +0000

    Creation of database tables with liquibase and SQL

--- src/test/java/com/bestinsurance/api/BestInsuranceApiApplicationTests.java
+++ src/test/java/com/bestinsurance/api/BestInsuranceApiApplicationTests.java
@@ -1,12 +1,14 @@
 package com.bestinsurance.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class BestInsuranceApiApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
